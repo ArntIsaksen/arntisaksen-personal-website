0ccccbde4c0729a55ea5f78e28d1ae3d44033886
arntisaksen <arnt.isaksen@gmail.com>
2017-02-23 23:27:19 +0100

    v.2.0
    Changed the overall structure, and added new content.

--- _js/addPrinciples.js
+++ _js/addPrinciples.js
@@ -9,7 +9,7 @@ function setup() {
 	var randomNr = floor(random(10));
 	var ruleText = principles[randomNr].rule;
 	var descriptionText = principles[randomNr].description;
-	var rule = createElement('h3', randomNr + '. ' + ruleText).class('subheader');
+	var rule = createElement('h3', (randomNr+1) + '. ' + ruleText).class('subheader');
 	var description = createElement('p', descriptionText);
 	var quoteFrom = createElement('p', '- Dieter Rams').class('text-right');
 	rule.parent('rule');
