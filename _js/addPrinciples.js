f72fa5af125cb7e2f1edab3994a43641adca0293
arntisaksen <arnt.isaksen@gmail.com>
2017-03-21 22:54:39 +0100

    Deployed version

--- _js/addPrinciples.js
+++ _js/addPrinciples.js
@@ -1,18 +1,18 @@
 var principles;
 
 function preload() {
-	principles = loadJSON('../_data/principles.json');
+    principles = loadJSON('/_data/principles.json');
 }
 
 function setup() {
-	noCanvas();
-	var randomNr = floor(random(10));
-	var ruleText = principles[randomNr].rule;
-	var descriptionText = principles[randomNr].description;
-	var rule = createElement('h3', (randomNr+1) + '. ' + ruleText).class('subheader');
-	var description = createElement('p', descriptionText);
-	var quoteFrom = createElement('p', '- Dieter Rams').class('text-right');
-	rule.parent('rule');
-	description.parent('principle-quote');
-	quoteFrom.parent('principle-quote');
+    noCanvas();
+    var randomNr = floor(random(10));
+    var ruleText = principles[randomNr].rule;
+    var descriptionText = principles[randomNr].description;
+    var rule = createElement('h3', (randomNr + 1) + '. ' + ruleText).class('subheader');
+    var description = createElement('p', descriptionText);
+    var quoteFrom = createElement('p', '- Dieter Rams').class('text-right');
+    rule.parent('rule');
+    description.parent('principle-quote');
+    quoteFrom.parent('principle-quote');
 }
